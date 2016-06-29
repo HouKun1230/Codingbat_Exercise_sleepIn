3e2466c300ba46eecefe3fc2ebe85b4fffa64c2c
KunHou <houkun1230@gmail.com>
2016-06-28 22:48:48 -0400

    first commit

---
+++ src/SleepIn/sleepIn.java
@@ -0,0 +1,15 @@
+package SleepIn;
+
+public class sleepIn {
+
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		  if(weekday == false || vacation == true)
+		  {
+		    return true;
+		  }else
+		  {
+		    return false;
+		  }
+		}
+	
+}
